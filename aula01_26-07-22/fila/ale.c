56058b5bf58e34dfb38558dcbf72cad114ed16b5
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-07-30 15:33:24 -0300

    Adicao_arquivos_feitos_em_sala
    Adição dos arquivos feitos em sala durante a primeira aula da disciplina.

---
+++ aula01_26-07-22/fila/ale.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct queue {
+    int items[100];
+    int front, rear;
+} qq;
+
+int main(void) {
+    struct queue variavel;
+    int vari;
+    qq variavelqueue;
+}
---
+++ aula01_26-07-22/fila/fila.c
@@ -0,0 +1,88 @@
+/*
+Lucas Silva de Almeida
+
+Enunciado:
+    Exercício de implementação das funções de uma fila.
+*/
+
+/******************************************************************************
+                          Diretrizes Pre-Processamento
+ ******************************************************************************/
+#include <stdio.h>
+
+#define QUEUEMAXLENGTH 100
+
+
+/*****************************************************************************
+                                    Structs
+ *****************************************************************************/
+typedef struct {
+    int items[QUEUEMAXLENGTH];
+    int front, rear;
+} queue;
+
+
+/****************************************************************************
+                                    Funcoes
+ ****************************************************************************/
+int emptyq(queue *ptq) {
+    if (ptq->front == ptq->rear)
+        return 1;
+    return 0;
+}
+
+int insertq(queue *ptq, int x) {
+    if (ptq->rear == QUEUEMAXLENGTH - 1) {
+        if (ptq->front == 0)
+            return -1;
+        ptq->rear = -1;
+    }
+    if (ptq->front == -1)
+        ptq->front = 0;
+    ptq->items[++ptq->rear] = x;
+    return 0;
+}
+
+int removeq(queue *ptq) {
+    if (emptyq(ptq))
+        return -1;
+    int x = ptq->items[ptq->front++];
+    if (ptq->front == QUEUEMAXLENGTH - 1)
+        ptq->front = 0;
+    return x;
+}
+
+int initializequeue(queue *ptq) {
+    ptq->front = -1;
+    ptq->rear = -1;
+    return 0;
+}
+
+
+/****************************************************************************
+                                 Funcao Principal
+ ****************************************************************************/
+int main(void) {
+    queue q;
+
+    initializequeue(&q);
+
+    if (emptyq(&q))
+        printf("A fila esta vazia! Vai agora.\n");
+    insertq(&q, 1);
+    insertq(&q, 2);
+    insertq(&q, 3);
+    insertq(&q, 4);
+    printf("Removido o numero: %d -- front = %d; rear = %d\n", removeq(&q), q.front, q.rear);
+    printf("Removido o numero: %d -- front = %d; rear = %d\n", removeq(&q), q.front, q.rear);
+    printf("Removido o numero: %d -- front = %d; rear = %d\n", removeq(&q), q.front, q.rear);
+    printf("Removido o numero: %d -- front = %d; rear = %d\n", removeq(&q), q.front, q.rear);
+    insertq(&q, 5);
+    printf("front: %d; rear: %d\n", q.front, q.rear);
+    insertq(&q, 6);
+    printf("front: %d; rear: %d\n", q.front, q.rear);
+    
+    getchar();
+
+    return 0;
+}
