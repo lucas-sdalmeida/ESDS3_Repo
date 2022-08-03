f9b296ef2bd400576eeb77bb7c0608b8902414e1
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-08-02 21:05:04 -0300

    Exercicio_Expressoes_Matematicas
    Resolução do exercício de validação do escopo de marcas de precedência (parênteses, colchetes e chaves) usando pilha.

--- aula01_26-07-22/expressoesmatematicas/main.c
+++ aula01_26-07-22/expressoesmatematicas/main.c
@@ -9,6 +9,7 @@ Enunciado:
                         Diretrizes Pre-Processamento
  ******************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STACKSMAXLENGTH 100
 
@@ -34,10 +35,83 @@ int empty(stack *pts) {
     return (pts->top >= 0) ? 0 : 1;
 }
 
+int push(stack *pts, int value) {
+    if (pts->top == STACKSMAXLENGTH - 1)
+        exit(-1);
+    pts->values[++pts->top] = value;
+    return 0;
+}
+
+char pop(stack *pts) {
+    if (empty(pts))
+        exit(-2);
+    return pts->values[pts->top--];
+}
+
+char stacktop(stack *pts) {
+    if (empty(pts))
+        exit(-2);
+    return pts->values[pts->top];
+}
+
+int linha(char caract, short int tam) {
+    int i=0;
+    for (; i<tam; i++)
+        putchar(caract);
+    putchar(10);
+    return 0;
+}
+
+int indexof(char *list, char character, int listlength) {
+    int i = 0;
+    for (; i<listlength; i++) {
+        if (character == list[i])
+            return i;
+    }
+    return -1;
+}
+
 
 /******************************************************************************
                                 Funcao Principal 
  ******************************************************************************/
 int main(void) {
+    stack precedencesmarks;
+    char expression[51], valida = 1; 
+    char openingmarks[] = {'(', '[', '{'}, closingmarks[] = {')', ']', '}'}; 
+    int i = 0, mark;
+
+    initializestack(&precedencesmarks);
+
+    linha('=', 60);
+    puts("Digite uma expressao algebrica para verificar se ela e valida: ");
+    linha('-', 30);
+    printf("\t>>> ");
+    fgets(expression, 51, stdin);
+    linha('-', 60);
+    do {
+        if (indexof(openingmarks, expression[i], 3) > -1) {
+            push(&precedencesmarks, expression[i]);
+            i++;
+            continue;
+        }
+        mark = indexof(closingmarks, expression[i], 3);
+        if (mark > -1) {
+            if (empty(&precedencesmarks)) {
+                valida = 0;
+                break;
+            }
+            if (indexof(openingmarks, stacktop(&precedencesmarks), 3) == mark)
+                pop(&precedencesmarks);
+        }
+        i++;
+    } while (expression[i]);
+    if (!empty(&precedencesmarks))
+        valida = 0;
+    printf("A expressao digita e %s!\n", (valida) ? "VALIDA" : "INVALIDA");
+    linha('=', 60);
+
+    getchar();
+
     return 0;
 }
