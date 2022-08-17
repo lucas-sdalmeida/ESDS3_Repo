87c082c5c69e59f1a5b439d0d6c7ea54950101db
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-08-16 21:25:37 -0300

    Comeco_resolucao
    Começo da resolução do exe01..

---
+++ aula04_16-08-22/exelistas01/main.c
@@ -0,0 +1,62 @@
+/*
+Lucas Silva de Almeida
+
+Enunciado:
+    Implementar uma lista encadeada de inteiros com as seguintes funcoes:
+        a. Inserir um elemento no início da lista.
+        b. Buscar um elemento na lista, retornando o seu endereço.
+        c. Apagar um elemento da lista passando o valor do elemento a ser apagado.
+        d. Imprimir a lista.
+        e. Ordenar a lista.
+        f. Inserir um elemento em uma lista ordenada, mantendo sua ordenacao
+*/
+
+
+/******************************************************************************
+                        Diretrizes Pre-Processamento
+ ******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/******************************************************************************
+                                   Estruturas
+ ******************************************************************************/
+typedef struct IntNode {
+    struct Node *nextNode;
+    int value;
+    int ordered;
+} IntNode, *IntList;
+
+
+/******************************************************************************
+                                    Funcoes
+ ******************************************************************************/
+int initializeList(IntList list) {
+    list = (IntNode*)malloc(sizeof(IntNode));
+    list->nextNode = NULL;
+    list->value = NULL;
+    return 0;
+}
+
+IntNode *searchElement(IntList list, int k);
+
+int insertElement(IntList list, int k);
+
+int removeElement(IntList list, int k);
+
+int showList(IntList list);
+
+int sortList(IntList list);
+
+
+/******************************************************************************
+                                Funcao Principal
+ ******************************************************************************/
+int main(void) {
+    IntList list;
+    
+    initializeList(list);
+
+    return 0;
+}
