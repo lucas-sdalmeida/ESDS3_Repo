86907eeb387efd90113ea7d8f3f4882abe51edcb
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-09-20 19:52:37 -0300

    Teste_Pilha
    Teste da pilha implementada.

--- estudo_pilha_fila_lista/main.c
+++ estudo_pilha_fila_lista/main.c
@@ -16,6 +16,8 @@ Descrição:
 #include "estruturas\queue.c"
 #include "estruturas\stack.c"
 
+#define STACK_MAX_LENGTH 100
+
 /******************************************************************************
                                     Funcoes 
  ******************************************************************************/
@@ -25,5 +27,26 @@ Descrição:
                                 Funcao Principal 
  ******************************************************************************/
 int main() {
+    stack *s = new_stack(STACK_MAX_LENGTH);
+
+    printf("pilha vazia? %d\n", is_empty(s));
+
+    int i = 0;
+    for (; i<10; i++)
+        push(s, i);
+
+    printf("pilha vazia? %d\n", is_empty(s));
+    puts("Elementos:");
+
+    for (i=0; i<10; i++)
+        printf("\t%d\n", pop(s));
+    
+    printf("pilha vazia? %d\n", is_empty(s));
+    push(s, 100);
+    printf("stack top: %d\n", stack_top(s));
+    printf("pilha vazia? %d\n", is_empty(s));
+
+    getchar();
+
     return 0;
 }
