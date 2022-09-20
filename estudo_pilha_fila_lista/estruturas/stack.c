61b216824ac1edfe827f6fa5551b27a385e6590d
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-09-20 19:41:32 -0300

    Implementacao_Pilha
    Implementação de uma pilha de inteiros a as funções para trabalhar com ela.

--- estudo_pilha_fila_lista/estruturas/stack.c
+++ estudo_pilha_fila_lista/estruturas/stack.c
@@ -1,1 +1,32 @@
 #include "stack.h"
+
+stack *new_stack(int len){
+    stack *the_new_stack = (stack*)malloc(sizeof(stack));
+    the_new_stack->length = len;
+    the_new_stack->top = -1;
+    the_new_stack->values = (int*)malloc(sizeof(int) * len);
+    return the_new_stack;
+}
+
+int push(stack *pts, int value) {
+    if (pts->top == pts->length - 1)
+        return -1;
+    pts->values[++pts->top] = value;
+    return 0;
+}
+
+int pop(stack *pts) {
+    if (is_empty(pts))
+        return -1;
+    return pts->values[pts->top--];
+}
+
+int is_empty(stack *pts) {
+    return pts->top == -1 ? 1 : 0;
+}
+
+int stack_top(stack *pts) {
+    if (is_empty(pts))
+        return -1;
+    return pts->values[pts->top];
+}
--- estudo_pilha_fila_lista/estruturas/stack.h
+++ estudo_pilha_fila_lista/estruturas/stack.h
@@ -2,11 +2,33 @@
 #define _STACK_H
 
 #include <stdlib.h>
+#include <math.h>
 
-struct stack {
+typedef struct stack {
     /*
+    Conjunto ordenado de itens no qual insercoes e remocoes so podem ser reali-
+    zadas em uma das extremidades. Tal extremidade e denominada "topo" da pilha.
     
+    Operacoes:
+        - Criar pilha;
+        - Adicionar item;
+        - Remover item, retornando-o
+        - Verificar se esta vazia;
+        - Obter o valor no topo da pilha sem remove-lo;
     */
-};
+    int *values;
+    int length;
+    int top;
+} stack;
+
+stack *new_stack(int len);
+
+int push(stack *pts, int value);
+
+int pop(stack *pts);
+
+int is_empty(stack *pts);
+
+int stack_top(stack *pts);
 
 #endif
