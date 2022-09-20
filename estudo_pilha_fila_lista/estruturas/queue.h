4425c581197794be0dcf89997095b0783b961c13
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-09-20 20:07:36 -0300

    Declaracao_Queue
    Declaração das características e principais funcionalidades da fila.

--- estudo_pilha_fila_lista/estruturas/queue.h
+++ estudo_pilha_fila_lista/estruturas/queue.h
@@ -3,10 +3,36 @@
 
 #include <stdlib.h>
 
-struct queue {
+typedef struct queue {
     /*
-    
-    */
-};
+    Estrutura de dados ordenada cuja a insercao ocorre em um extremidade somente
+    e a remocao ocorre em outra. Respectivamente, fim da fila (rear) e inicio da
+    fila (front)
 
+    Caracteristicas:
+        - Valores;
+        - Tamanho;
+        - Indice do inicio da fila;
+        - Indice do fim da fila;
+    
+    Operacoes:
+        - Criar fila;
+        - Inserir valor;
+        - Remover valor;
+        - Verificar se esta vazia;
+    */
+
+    int *values;
+    int length;
+    int front, rear;
+} queue;
+
+
+queue *new_queue(int length);
+
+int insert(queue *ptq, int value);
+
+int remove(queue *ptq);
+
+int is_empty(queue *ptq);
 #endif
